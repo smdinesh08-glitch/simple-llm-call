6cbb4b196e4403452246c511ca7717000d37c324
Dinesh <smdinesh08@gmail.com>
2025-12-07 10:40:30 +0530

    code update

---
+++ main.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+generator = pipeline("text-generation",model="distilgpt2") # used in huggingface 
+
+result =generator(
+    " AI is the future because ",
+    max_length = 50,
+    num_return_sequences =1
+)
+
+print(result[0]["generated_text"])
